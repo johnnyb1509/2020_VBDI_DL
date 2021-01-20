6106b406c6194e38edafdd18462c1ff673a3e618
Nguyễn Minh Sơn (VNCDLL-CTÐTKSAI) <150994johnnyb@gmail.com>
2021-01-20 09:52:25 +0700

    fix beam

--- src/beam_search.py
+++ src/beam_search.py
@@ -36,7 +36,7 @@ import math
 #         if beam_decoder.sentence_length['sen{}'.format(num_sen)] >= MAX_SUMMARY_LENGTH:
 #             stop_condition = True
     
-
+""" Main code """
 class Beam_Decoder():
     def __init__(self, x_tokenizer, y_tokenizer,
                          decoder_model, encoder_output, max_summary_len,
@@ -166,27 +166,27 @@ class Beam_Decoder():
 
 
 
-def beam_search_decoder(predictions, top_k = 3):
-    #start with an empty sequence with zero score
-    output_sequences = [([], 0)]
+# def beam_search_decoder(predictions, top_k = 3):
+#     #start with an empty sequence with zero score
+#     output_sequences = [([], 0)]
     
-    #looping through all the predictions
-    for token_probs in predictions:
-        new_sequences = []
+#     #looping through all the predictions
+#     for token_probs in predictions:
+#         new_sequences = []
         
-        #append new tokens to old sequences and re-score
-        for old_seq, old_score in output_sequences:
-            for char_index in range(len(token_probs)):
-                new_seq = old_seq + [char_index]
-                #considering log-likelihood for scoring
-                new_score = old_score + math.log(token_probs[char_index])
-                new_sequences.append((new_seq, new_score))
+#         #append new tokens to old sequences and re-score
+#         for old_seq, old_score in output_sequences:
+#             for char_index in range(len(token_probs)):
+#                 new_seq = old_seq + [char_index]
+#                 #considering log-likelihood for scoring
+#                 new_score = old_score + math.log(token_probs[char_index])
+#                 new_sequences.append((new_seq, new_score))
                 
-        #sort all new sequences in the de-creasing order of their score
-        output_sequences = sorted(new_sequences, key = lambda val: val[1], reverse = True)
+#         #sort all new sequences in the de-creasing order of their score
+#         output_sequences = sorted(new_sequences, key = lambda val: val[1], reverse = True)
         
-        #select top-k based on score 
-        # *Note- best sequence is with the highest score
-        output_sequences = output_sequences[:top_k]
+#         #select top-k based on score 
+#         # *Note- best sequence is with the highest score
+#         output_sequences = output_sequences[:top_k]
         
-    return output_sequences
+#     return output_sequences
